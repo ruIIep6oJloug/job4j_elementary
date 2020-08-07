93f509ccdf4e1421437844f54103c7eefd777f6c
DELL <prof666@bk.ru>
2020-08-07 20:56:49 +0500

    Фиксация изменений на GitHub. [#243564]

--- src/ru/job4j/Studentinfo.java
+++ src/ru/job4j/Studentinfo.java
@@ -1,4 +1,8 @@
 package ru.job4j;
 
 public class Studentinfo {
+    public static void main(String [] args) {
+        System.out.println("Popov Yuriy");
+        System.out.println("11.04.1988");
+    }
 }
