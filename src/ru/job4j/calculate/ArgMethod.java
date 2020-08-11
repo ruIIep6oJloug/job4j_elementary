4ef250f474b0556303d9dad4e59a3d11e7897686
DELL <prof666@bk.ru>
2020-08-11 23:13:26 +0500

    4.2

---
+++ src/ru/job4j/calculate/ArgMethod.java
@@ -0,0 +1,13 @@
+package ru.job4j.calculate;
+
+public class ArgMethod {
+    public static void hello (String age){
+       System.out.println(age);
+    }
+    public static void main(String[] args) {
+        String name = "Yuriy Popov";
+        String age = "32";
+         ArgMethod.hello(age);
+             }
+
+}
