4048cf96abfa4101f1cd9204d08f7749e8a17f0d
DELL <prof666@bk.ru>
2020-08-14 21:57:43 +0500

    9.1 String

---
+++ src/ru/job4j/calculate/Out.java
@@ -0,0 +1,14 @@
+package ru.job4j.calculate;
+
+public class Out {
+    public static void main(String[] args){
+        String lyrics = "Мама мыла раму";
+        System.out.println(lyrics);
+        String name = "Petr";
+        byte age = 33;
+        String surname = "Arsentev";
+        String separate = " - ";
+        String id = name + separate + age + " years old";
+        System.out.println(id);
+    }
+}
---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,10 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args){
+        int year = 2020;
+        String idea = "I like Java! But I am a newbie. " + year;
+        System.out.println(idea);
+    }
+
+}
