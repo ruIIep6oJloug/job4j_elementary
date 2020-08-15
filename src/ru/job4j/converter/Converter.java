dc669e1675560a12d34df99b109ad35a50544c71
DELL <prof666@bk.ru>
2020-08-15 22:50:38 +0500

    0. Тестирование

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,18 +1,24 @@
 package ru.job4j.converter;
 
 public class Converter {
-    public static int rubleToEuro(int value){
-        int rsl = value / 70;
-        return rsl;
+    public static int rubleToEuro(int value) {
+         return value / 70;
     }
-    public static double rubleToDollar(double value){
-        double rsl = value / 60;
-        return rsl;
+
+    public static int rubleToDollar(int value) {
+        return value / 60;
     }
-    public static void main(String[] args){
-        int euro = Converter.rubleToEuro(140);
-        System.out.println("140 rubles are " + euro + " euro.");
-        double dollar = Converter.rubleToDollar(140);
-        System.out.println("140 rubles are " + dollar + " dollar.");
+
+    public static void main(String[] args) {
+        int in = 140;
+        int expected = 2;
+        int out = rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+        int in2 = 140;
+        int expected2 = 2;
+        int out2 = rubleToDollar(in2);
+        boolean passed2 = expected2 == out2;
+        System.out.println("140 rubles are 2. Test result : " + passed2);
     }
 }
