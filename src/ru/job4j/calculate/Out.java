cf177a594b377e6dd74f7f8749ec310ea2308e8e
DELL <prof666@bk.ru>
2020-08-15 22:22:19 +0500

    9.1 String

--- src/ru/job4j/calculate/Out.java
+++ src/ru/job4j/calculate/Out.java
@@ -1,7 +1,7 @@
 package ru.job4j.calculate;
 
 public class Out {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String lyrics = "Мама мыла раму";
         System.out.println(lyrics);
         String name = "Petr";
