c60fcc3d540b9682aa2de6ac4b1b0b2925010c57
DELL <prof666@bk.ru>
2020-09-01 22:17:33 +0500

    4.3.4. if с return

---
+++ test/ru/job4j/condition/LogicNotTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.condition;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LogicNotTest extends TestCase {
+
+    @Test
+    public void isEven() {
+    }
+
+    @Test
+    public void isPositive() {
+    }
+
+    @Test
+    public void notEven() {
+    }
+
+    @Test
+    public void notPositive() {
+    }
+
+    @Test
+    public void notEvenAndPositive() {
+    }
+
+    @Test
+    public void evenrNotPositive() {
+    }
+}
