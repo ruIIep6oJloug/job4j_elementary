cf47ac5b228cc228f4d87bdfbd8a83ca91c3b7f5
DELL <prof666@bk.ru>
2020-09-08 18:42:06 +0500

    4.5. Отладка прграммы в IDEA

--- src/ru/job4j/condition/MultiMax.java
+++ src/ru/job4j/condition/MultiMax.java
@@ -2,7 +2,7 @@ package ru.job4j.condition;
 
 public class MultiMax {
     public static int max(int first, int second, int third) {
-        int result = first;
+        int result = third;
         if (first > second && third < first) {
             result = first;
         }
