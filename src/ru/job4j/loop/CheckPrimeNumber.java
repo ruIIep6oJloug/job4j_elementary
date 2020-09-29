b02c640c5626b7fda8f3e8a2615475c6ee51a61c
DELL <prof666@bk.ru>
2020-09-29 22:33:04 +0500

    5.5. Простое число

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,18 +3,22 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
     public static boolean check(int number) {
         boolean prime = true;
+        if (number == 1) {
+            prime = false;
+        }
         for (int index = 2; index < number; index++) {
             if ((number % index) == 0) {
                 prime = false;
                 break;
+            } else {
+                prime = true;
             }
         }
         return prime;
     }
 
     public static void main(String[] args) {
-        System.out.println(check(3));
-        System.out.println(check(5));
-        System.out.println(check(9));
+
+        System.out.println(check(1));
     }
 }
