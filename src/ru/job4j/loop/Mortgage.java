ca004a178e91b372c0c41a078cbcc497f4836d9b
DELL <prof666@bk.ru>
2020-10-13 20:35:41 +0500

    5.7. Ипотека

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -3,14 +3,16 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(int amount, int salary, double percent) {
         int year = 0;
-            while (amount < salary) {
+        while ((amount < salary) & (amount > 0)) {
+            amount = (int) (((amount / 100) * percent) + amount - salary);
+            if (amount < salary) {
                 year++;
-                amount = ((amount / 100) * percent) + amount - salary;
             }
+        }
         return year;
     }
 
     public static void main(String[] args) {
-        year(1000, 1200, 1);
+        System.out.println(Mortgage.year(100, 120, 50));
     }
 }
