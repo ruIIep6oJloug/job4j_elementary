ad945e7ca5c7d6b373d0ebcebfb2266409cdb445
DELL <prof666@bk.ru>
2020-10-21 22:53:13 +0500

    6.0.3.1. Массивы и цикл for-earch

---
+++ src/ru/job4j/array/ForEarchUsege.java
@@ -0,0 +1,10 @@
+package ru.job4j.array;
+
+public class ForEarchUsege {
+    public static void main(String[] args) {
+        String[] numbers = {"one", "two", "three", "four", "five"};
+        for (String i : numbers) {
+            System.out.println(i);
+        }
+    }
+}
