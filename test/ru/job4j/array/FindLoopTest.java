cc40a303840a6db3bf59496e52bd92ac1bad95e9
DELL <prof666@bk.ru>
2020-10-23 19:33:10 +0500

    6.2. Упорядочить массив

--- test/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/FindLoopTest.java
@@ -9,9 +9,9 @@ public class FindLoopTest {
     @Test
     public void qhenArrayHas5Then0() {
         int input[] = {5, 4, 3, 2};
-        int value = 5;
+        int value = 3;
         int result = FindLoop.indexOf(input, value);
-        int expect = 0;
+        int expect = 2;
         assertThat(result, is(expect));
     }
 }
