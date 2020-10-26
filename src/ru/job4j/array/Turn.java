8084f4a50c4ba6c989fa3036e1a142bf7d20a5f3
DELL <prof666@bk.ru>
2020-10-26 22:27:44 +0500

    6.2.3. Перевернуть массив

---
+++ src/ru/job4j/array/Turn.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+
+public class Turn {
+    public static int[] back(int[] array) {
+        for (int index = 0; index < array.length - 1; index++) {
+        int temp = array[index];
+        array[index] = array[(array.length - index) - 1];
+        array[(array.length - index) - 1] = temp;
+            if (index == (array.length / 2)) {
+                break;
+            } else {
+                if (index == (array.length - 1) / 2) {
+                break;
+                }
+            }
+
+        }
+        return array;
+    }
+}
