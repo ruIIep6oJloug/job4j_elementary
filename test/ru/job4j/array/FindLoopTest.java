e45f92b07460b88dec4e9d257ce916dd511ba19c
DELL <prof666@bk.ru>
2020-10-28 18:45:55 +0500

    6.5. Поиск индекса в диапазоне

--- test/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/FindLoopTest.java
@@ -14,4 +14,26 @@ public class FindLoopTest {
         int expect = 2;
         assertThat(result, is(expect));
     }
+
+    @Test
+    public void rangeTrue() {
+        int input[] = {5, 4, 3, 2};
+        int value = 3;
+        int start = 1;
+        int finish = 3;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 2;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void rangeFalse() {
+        int input[] = {5, 4, 3, 2};
+        int value = 6;
+        int start = 1;
+        int finish = 3;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
 }
