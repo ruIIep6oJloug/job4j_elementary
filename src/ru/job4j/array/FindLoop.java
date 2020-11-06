1c97040f11726972fe670a591be6c72bf6a517f0
DELL <prof666@bk.ru>
2020-11-06 16:42:45 +0500

    6.5.3. Сортировка выборкой

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -1,17 +1,6 @@
 package ru.job4j.array;
 
 public class FindLoop {
-    public static int indexOf(int[] data, int el) {
-        int rst = -1;
-        for (int index = 0; index < el; index++) {
-            if (data[index] == el) {
-                rst = index;
-                break;
-            }
-        }
-        return rst;
-    }
-
     public static int indexOf(int[] data, int el, int start, int finish) {
         int rst = -1;
         for (int index = start; index <= finish; index++) {
