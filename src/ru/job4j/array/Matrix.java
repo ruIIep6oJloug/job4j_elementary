1a00136ec3dff6a8c6d8608f0104ef6917404d54
DELL <prof666@bk.ru>
2020-11-10 22:01:55 +0500

    6.6.8. Двухмерный массив. Таблица умнажения.

---
+++ src/ru/job4j/array/Matrix.java
@@ -0,0 +1,13 @@
+package ru.job4j.array;
+// 6.6.8. Двухмерный массив. Таблица умнажения.
+public class Matrix {
+    public static int[][] multiple(int size) {
+        int[][] table = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                table[i][j] = (i + 1) * (j + 1);
+            }
+        }
+        return table;
+    }
+}
